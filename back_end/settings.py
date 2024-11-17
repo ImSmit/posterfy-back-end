9e76b5a303f8d8c6e5cd7cd1f3a5d9c97a92bbab
smit patel <sempatel348@gmail.com>
2024-11-17 12:54:14 +0530

    .

--- back_end/settings.py
+++ back_end/settings.py
@@ -138,22 +138,22 @@ WSGI_APPLICATION = 'back_end.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
-        "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "posterfy",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432", # default PostgreSQL port
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#         "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "posterfy",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "localhost",
+#         "PORT": "5432", # default PostgreSQL port
+#     }
+# }
 
 
 # Password validation
